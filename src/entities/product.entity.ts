03116bf464b43d298d7aee9ca4c97c109fc0bcf5
redolf250 <55606159+redolf250@users.noreply.github.com>
2025-11-16 11:04:22 +0000

    "Updated product entity with new columns and TypeORM imports."

--- src/entities/product.entity.ts
+++ src/entities/product.entity.ts
@@ -1,6 +1,20 @@
-import { Entity } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity()
+@Entity("products")
 export class Product {
 
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: "varchar", length: 10, unique: true, nullable: false, update: false })
+  skuCode: string;
+
+  @Column({ type: "varchar", length: 50, nullable: false })
+  productName: string;
+
+  @Column({ type: "int", nullable: false })
+  quantity: number;
+
+  @Column({ type: "decimal", precision: 10, scale: 2, nullable: false})
+  unitPrice: number;
 }
