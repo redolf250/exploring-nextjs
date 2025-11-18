136a97ec80a27e1d3a57fbec1a0f435370085082
redolf250 <55606159+redolf250@users.noreply.github.com>
2025-11-18 17:44:25 +0000

    "Added payment gateway support for users controller"

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -1,8 +1,17 @@
-import { Body, Controller, Get, Post, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { UsersService } from '../services/users.service';
 import { UserEntity } from '../entities/user.entity';
-import { Interceptor } from '../../commons/Interceptor';
-
+import { MobileMoneyGateway } from '../../integrations/payments/MobileMoneyGateway';
+import { VisaCardGateway } from '../../integrations/payments/VisaCardGateway';
+import { randomUUID } from 'node:crypto';
 
 
 @Controller('users')
@@ -13,6 +22,22 @@ export class UsersController {
   @Get("/all")
   async getAllUsers(){
       return await this.usersService.findAll();
+  }
+
+  @Put()
+  async makePayment(@Body() body : any) {
+    const gateways: Map<string, PaymentGateway> = new Map();
+    gateways.set("mtn_momo", new MobileMoneyGateway());
+    gateways.set("visa_card", new VisaCardGateway());
+    const {gatewayType} = body
+    const gateway = gateways.get(gatewayType)
+    
+    if (!gateway)
+      throw new NotFoundException(`Gateway not found ${gatewayType}`);
+    gateway.initializePayment()
+    gateway.verifyTransaction()
+
+    return {trxId: randomUUID().toString(), message: "Payment was verified."};
   }
 
   @Post()
