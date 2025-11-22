aba7145d4bdfe49f1ee3205ba3303f1c3a85ca02
redolf250 <55606159+redolf250@users.noreply.github.com>
2025-11-22 16:25:42 +0000

    Create digital-signature.service.ts

---
+++ src/digital-signature/digital-signature.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@nestjs/common';
+import * as crypto from 'crypto';
+import { SignatureDto } from './signature.dto';
+
+@Injectable()
+export class DigitalSignatureService {
+
+   generateKeyPair() {
+    const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa', {
+      modulusLength: 2048, // Key size
+      publicKeyEncoding: {
+        type: 'spki',
+        format: 'pem',
+      },
+      privateKeyEncoding: {
+        type: 'pkcs8',
+        format: 'pem',
+        cipher: 'aes-256-cbc',
+        passphrase: 'top secret',
+      },});
+    return { publicKey: publicKey, privateKey: privateKey };
+  }
+
+  createSignature(body: SignatureDto) {
+     const { fullName, creation, expiration } = body;
+     const data =  `fullName=${fullName}
+                    creation=${creation}
+                    expiration=${expiration}`;
+     const {privateKey, publicKey} = this.generateKeyPair()
+     const signature = crypto.sign("sha256", Buffer.from(data),
+       {
+         key: privateKey, // The PEM string or key object
+         passphrase: 'top secret' // <--- CRITICAL: Must be correct if the key is encrypted
+       });
+     const signatureHex = signature.toString("hex");
+     return {publicKey: publicKey, signature: signatureHex};
+  }
+
+  verifySignature(body: SignatureDto) {
+    const { fullName, creation, expiration, publicKey, signature } = body;
+     const data =  `fullName=${fullName}
+                    creation=${creation}
+                    expiration=${expiration}`;
+     const isValid = crypto.verify(
+      "sha256",
+      Buffer.from(data),
+      publicKey,
+      Buffer.from(signature, "hex")
+    );
+     return {isValid: isValid, signature: signature};
+  }
+
+
+}
