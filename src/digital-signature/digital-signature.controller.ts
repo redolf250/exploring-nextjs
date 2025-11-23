56a4b94c61ca95ed5e4438b0e2dba2d950b8804b
redolf250 <55606159+redolf250@users.noreply.github.com>
2025-11-23 22:28:36 +0000

    "Added @Req() to Post method for HMAC generation"

--- src/digital-signature/digital-signature.controller.ts
+++ src/digital-signature/digital-signature.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req } from '@nestjs/common';
 import { DigitalSignatureService } from './digital-signature.service';
 import { SignatureDto } from './signature.dto';
 
@@ -22,4 +22,34 @@ export class DigitalSignatureController {
   verifySignature(@Body() data: SignatureDto) {
     return this.digitalSignatureService.verifySignature(data);
   }
+
+  @Post('/hmac')
+  generateHmac(@Body() data: SignatureDto, @Req() req: Request) {
+    return this.digitalSignatureService.generateHmac(data);
+  }
+
+  // @Post()
+  // handleWebhook(@Req() req: Request, @Res() res: Response) {
+  //   const signature = req.headers['x-signature'] as string;
+  //
+  //   if (!signature) {
+  //     return res.status(HttpStatus.BAD_REQUEST).send('Missing signature');
+  //   }
+  //
+  //   if (!verifyHmac(req.rawBody, signature, SECRET)) {
+  //     return res.status(HttpStatus.UNAUTHORIZED).send('Invalid signature');
+  //   }
+  //
+  //   // If signature is valid, process payload
+  //   console.log('Verified payload:', req.body);
+  //   return res.status(HttpStatus.OK).send('Webhook verified');
+  // }
+
+  // export function verifyHmac(rawBody: Buffer, signature: string, secret: string): boolean {
+  //   const computed = crypto.createHmac('sha256', secret)
+  //     .update(rawBody)
+  //     .digest('hex');
+  //
+  //   return crypto.timingSafeEqual(Buffer.from(signature), Buffer.from(computed));
+  // }
 }
